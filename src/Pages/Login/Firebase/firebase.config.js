73362e84d19f3cd509f1c98060c440dc8b9ae649
rased100 <rased100@gmail.com>
2022-01-14 23:47:57 +0600

    first commit

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// console.log(process.env);
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// };
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDvtPXK6buSa0uM5hZXLrZHyK3USkUW7VA",
+    authDomain: "genius-car-mechanics-3ffbe.firebaseapp.com",
+    projectId: "genius-car-mechanics-3ffbe",
+    storageBucket: "genius-car-mechanics-3ffbe.appspot.com",
+    messagingSenderId: "839883349262",
+    appId: "1:839883349262:web:6d002d65e5745757fccb5f"
+};
+
+export default firebaseConfig;
